14d481f64fdb815c7d349825cabe2e22ba98db24
Shubham Mankar <mankarshubham81@gmail.com>
2024-03-26 14:29:17 +0530

    settled node vesrion range in production server

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT;
 // const PORT = process.env.PORT || 4300;
 connectDB();
 
-console.log("aaa", process.env.PORT);
-console.log("aaa", process.env.MONGO_URI);
+console.log("port: ", process.env.PORT);
+console.log("mongo uri: ", process.env.MONGO_URI);
 
 app.listen(PORT, () => {
     console.log(`Server is workong on port ${PORT} in ${process.env.NODE_ENV} Mode`);
